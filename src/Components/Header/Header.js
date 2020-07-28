f397487b94b4cd72fffd4c786a3863cf28652dcd
melaniesarah <melaniesarah@gmail.com>
2020-07-27 19:50:53 -0500

    Add header preliminary css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import Logo from './logo.png';
+import './Header.css';
 
 function Header() {
-    return <h2>I'm your Header</h2>;
+    return (
+      <header className="header">
+        <div className="header-logo">
+          <Link to="/">
+            <img src={Logo} alt="Website Logo" className="logo" />
+          </Link>
+        </div>
+        <nav className="header-nav">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <div className="dropdown">
+            <button class="dropbtn">Performing Groups</button>
+            <div class="dropdown-content">
+              <NavLink to="/groups/wind">Wind Band</NavLink>
+              <NavLink to="/groups/orchestra">Orchestra</NavLink>
+              <NavLink to="/groups/jazz">Jazz Band</NavLink>
+              <NavLink to="/groups/chamber">Chamber Ensembles</NavLink>
+              <NavLink to="/groups/marching">Marching Band</NavLink>
+            </div>
+          </div>
+          <NavLink to="/multimedia">Film and Media</NavLink>
+          <NavLink to="/appearances">Guest Appearances</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+      </header>
+    );
 }
 
 export default Header;
