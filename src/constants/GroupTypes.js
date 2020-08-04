bdc54fc3a423401e99c92433df523e2c8ea3dc21
melaniesarah <melaniesarah@gmail.com>
2020-08-04 18:36:04 -0500

    Add group type contants

---
+++ src/constants/GroupTypes.js
@@ -0,0 +1,28 @@
+
+export const GROUP_LIST = [WIND, CHAMBER, JAZZ, ORCHESTRA, MARCHING];
+
+export const WIND = { 
+    id: 'f8cdd714-49ef-43d2-8c2e-9e9606a2bca6',
+    title: 'Wind Band'
+};
+
+export const CHAMBER = {
+    id: 'a02f20df-803a-42bb-b092-2d4be41dc3e7',
+    title: 'Chamber Ensembles'
+};
+
+export const JAZZ = {
+    id: '5d30ef6c-ffbf-42cd-bea7-b587519d4e2f',
+    title: 'Jazz Band'
+}
+
+export const ORCHESTRA = {
+    id: '54c80182-2a74-45dd-adfc-830f1ead5399',
+    title: 'Orchestra'
+}
+
+export const MARCHING = {
+    id: '4afa6fa2-ce8f-470b-86d7-62fa04461423',
+    title: 'Marching Band'
+}
+
