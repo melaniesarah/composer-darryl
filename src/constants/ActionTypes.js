911d460e6307d3653d03c8db1923c14713492603
melaniesarah <melaniesarah@gmail.com>
2020-08-08 19:39:34 -0500

    Constants for action types

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,26 @@
+//group-specific actions
+export const ADD_SONG = 'ADD_SONG';
+export const EDIT_SONG = 'EDIT_SONG';
+export const DELETE_SONG = 'DELETE_SONG';
+export const SONG_ACTIONS = [ADD_SONG, EDIT_SONG, DELETE_SONG ];
+export const EDIT_COMING_SOON = 'EDIT_COMING_SOON';
+
+//guest appearance actions
+export const ADD_APPEARANCE = 'ADD_APPEARANCE';
+export const EDIT_APPEARANCE = 'EDIT_APPEARANCE';
+export const DELETE_APPEARANCE = 'DELETE_APPEARANCE';
+export const APPEARANCE_ACTIONS = [ADD_APPEARANCE, EDIT_APPEARANCE, DELETE_APPEARANCE]
+
+// multimedia track actions
+export const ADD_TRACK = 'ADD_TRACK';
+export const EDIT_TRACK = 'EDIT_TRACK';
+export const DELETE_TRACK = 'DELETE_TRACK';
+export const TRACK_ACTIONS = [ADD_TRACK, EDIT_TRACK, DELETE_TRACK]
+
+//home page actions
+export const EDIT_PRESS = 'EDIT_PRESS';
+export const EDIT_LATEST = 'EDIT_LATEST';
+export const EDIT_BIO = 'EDIT_BIO';
+
+//CONTACT PAGE
+export const EDIT_CONTACT_BULLETS = 'EDIT_CONTACT_BULLETS';
